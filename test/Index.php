9a2ea986db03198be87fdcd70a438f69ef307c4d
Christian Blanquera <cblanquera@openovate.com>
2015-09-23 19:30:46 +0800

    Initial Commit

---
+++ test/Index.php
@@ -0,0 +1,21 @@
+<?php //-->
+/*
+ * This file is part of the Model package of the Eden PHP Library.
+ * (c) 2013-2014 Openovate Labs
+ *
+ * Copyright and license information can be found at LICENSE
+ * distributed with this package.
+ */
+ 
+class EdenServerIndexTest extends PHPUnit_Framework_TestCase
+{
+    public function testSet() 
+    {
+		
+	}
+	
+	public function testGet() 
+    {
+		
+	}
+}
